9bba211caadf90584f478d756f46e63d574ca08a
Sahit Penmatcha <sahilitrix123@gmail.com>
2016-11-06 11:07:13 -0500

    Initial Commit

---
+++ Flic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flic
+//
+//  Created by Sahit Penmatcha on 11/6/16.
+//  Copyright © 2016 Sahit Penmatcha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
